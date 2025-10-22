398a8c27ff150dae7561d7c1742848f06251a338
Fabiana Barahona <fabiana.barahona1@gmail.com>
2025-10-22 14:05:44 +0000

    finance campaign month

---
+++ models/mart/finance/finanace_campaigns_month.sql
@@ -0,0 +1,18 @@
+SELECT
+    EXTRACT(month FROM date_date) AS date_month,
+    SUM(ads_margin) AS ads_margin,
+    SUM(average_basket) AS average_basket,
+    SUM(operational_margin) AS operational_margin,
+    SUM(ads_cost) AS ads_cost,
+    SUM(ads_impression) AS ads_impression,
+    SUM(ads_click) AS ads_click,
+    SUM(quantity) AS quantity,
+    SUM(revenue) AS revenue,
+    SUM(purchase_cost) AS purchase_cost,
+    SUM(margin) AS margin,
+    SUM(shipping_fee) AS shipping_fee,
+    SUM(log_cost) AS log_cost,
+    SUM(ship_cost) AS ship_cost
+FROM 
+    {{ ref('finance_campaigns_day') }} 
+GROUP BY date_month
