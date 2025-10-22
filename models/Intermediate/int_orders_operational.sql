f7c2b0c4afe3a5672e6111be1086e6eae8b7b3b8
Fabiana Barahona <fabiana.barahona1@gmail.com>
2025-10-22 09:10:47 +0000

    operatoinal_margiin_v2

--- models/Intermediate/int_orders_operational.sql
+++ models/Intermediate/int_orders_operational.sql
@@ -1,10 +1,10 @@
 SELECT
-m.orders_id
-, m.date_date
-, ROUND(m.margin + s.shipping_fee - logCost - ship_cost, 2) AS operational_margin
-, m.revenue AS revenue
-, m.quantity AS quantity
-FROM {{ ref('int_orders_margin') }} AS m
-LEFT JOIN {{ ref('stg_raw__ship') }} AS s
-USING (orders_id)
-ORDER BY date_date DESC , orders_id DESC
+int_margin.*
+, stg_ship.shipping_fee AS shipping_fee
+, stg_ship.logCost AS log_cost
+, stg_ship.ship_cost AS ship_cost
+, ROUND((int_margin.margin + stg_ship.shipping_fee - stg_ship.logCost - CAST(stg_ship.ship_cost AS FLOAT64)), 2) AS operational_margin
+FROM {{ ref('int_orders_margin')}} AS int_margin
+LEFT JOIN {{ ref('stg_raw__ship')}} AS stg_ship
+USING(orders_id)
+ORDER BY orders_id DESC
